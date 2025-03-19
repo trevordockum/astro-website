4f83b4e5001679dc1aef297198ed7fe767e9ce6a
Trevor Dockum <trevordock3@gmail.com>
2025-03-19 12:37:48 -0500

    Added Contact Form

---
+++ header.js
@@ -0,0 +1,35 @@
+new FinisherHeader({
+    "count": 300,
+    "size": {
+      "min": 2,
+      "max": 8,
+      "pulse": 0.3
+    },
+    "speed": {
+      "x": {
+        "min": 0,
+        "max": 0.4
+      },
+      "y": {
+        "min": 0,
+        "max": 0.6
+      }
+    },
+    "colors": {
+      "background": "#201e30",
+      "particles": [
+        "#fbfcca",
+        "#d7f3fe",
+        "#ffd0a7"
+      ]
+    },
+    "blending": "overlay",
+    "opacity": {
+      "center": 0.9,
+      "edge": 0.35
+    },
+    "skew": 0,
+    "shapes": [
+      "c"
+    ]
+  });
